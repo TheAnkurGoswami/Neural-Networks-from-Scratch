08e4ccc5e9fe7c523ace3b54b7b883d97ec4d884
TheAnkurGoswami <kinglusifer1401@gmail.com>
2022-11-12 23:47:17 +0530

    rmsprop

--- neural_networks/optimizers.py
+++ neural_networks/optimizers.py
@@ -1,4 +1,4 @@
-from typing import Any, Type
+from typing import Any, Tuple, Type
 
 import numpy as np
 
@@ -8,7 +8,8 @@ class Optimizer:
         pass
 
     def optimize(
-            self, history: np.ndarray, derivative: np.ndarray) -> np.ndarray:
+            self, history: np.ndarray,
+            derivative: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         raise NotImplementedError()
 
 
@@ -18,7 +19,8 @@ class SGD(Optimizer):
         self._momentum = momentum
 
     def optimize(
-            self, history: np.ndarray, derivative: np.ndarray) -> np.ndarray:
+            self, history: np.ndarray,
+            derivative: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """
         The original implementation for stochastic gradient descent is
         as follows:
@@ -37,11 +39,32 @@ class SGD(Optimizer):
         """
         new_change = self._momentum * history + \
             self._learning_rate * derivative
-        return new_change
+        return new_change, new_change
+
+
+class RMSProp(Optimizer):
+    def __init__(
+            self,
+            learning_rate: float,
+            rho: float,
+            epsilon: float = 1e-07) -> None:
+        self._learning_rate = learning_rate
+        self._rho = rho
+        self._epsilon = epsilon
+
+    def optimize(
+            self, history: np.ndarray,
+            derivative: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        new_history = self._rho * history + \
+            (1 - self._rho) * np.square(derivative)
+        new_change = (self._learning_rate * derivative) / (
+            np.sqrt(new_history) + self._epsilon)
+        return new_change, new_history
 
 
 def get_optimizer(optimizer: str) -> Type[Optimizer]:
     optimizer_map = {
         "sgd": SGD,
+        "rmsprop": RMSProp,
     }
     return optimizer_map[optimizer]
--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -3,21 +3,36 @@ from typing import Any, Dict
 import numpy as np
 import pytest
 import tensorflow as tf
+import torch
 
 from neural_networks.losses import RMSELoss
 from neural_networks.nn import Dense
 from neural_networks.optimizers import get_optimizer
 
+TF_OPTIM_MAP = {
+    "sgd": tf.keras.optimizers.SGD,
+    "rmsprop": tf.keras.optimizers.RMSprop,
+}
 
-@pytest.mark.parametrize("optimizer_str", ["sgd"])
-@pytest.mark.parametrize("kwargs", [{"learning_rate": 0.001, "momentum": 0.9}])
+TORCH_OPTIM_MAP = {
+    "sgd": torch.optim.SGD,
+    "rmsprop": torch.optim.RMSprop,
+}
+
+
+@pytest.mark.parametrize(
+    "optimizer_str, kwargs",
+    [
+        ("sgd", {"learning_rate": 0.001, "momentum": 0.9}),
+        ("rmsprop", {"learning_rate": 0.001, "rho": 0.9, "epsilon": 1e-07})
+    ])
 def test_n_hidden_layer_simple_nn(
         optimizer_str: str, kwargs: Dict[str, Any]) -> None:
     epochs = 10
-    np.random.seed(78)
+    np.random.seed(65)
 
     x = np.random.randint(low=0, high=10, size=(1, 5))
-    y = np.random.randint(low=0, high=10, size=(1, ))
+    y = np.random.randint(low=0, high=10, size=(1, 1))
 
     hidden_layers_size = [3, 12, 7]
     layers = [x.shape[1]] + hidden_layers_size + [1]
@@ -25,6 +40,8 @@ def test_n_hidden_layer_simple_nn(
     dense_layers = []
     tf_weights_list = []
     tf_biases_list = []
+    torch_weights_list = []
+    torch_biases_list = []
     optimizer = get_optimizer(optimizer_str)(**kwargs)
     for idx in range(n_layers):
         dense = Dense(in_features=layers[idx], out_features=layers[idx+1])
@@ -33,15 +50,34 @@ def test_n_hidden_layer_simple_nn(
         dense_layers.append(dense)
 
         w_tf = tf.Variable(w.astype(np.float32))
-        b_tf = tf.Variable(b, dtype=tf.float32)
+        b_tf = tf.Variable(b.astype(np.float32))
         tf_weights_list.append(w_tf)
         tf_biases_list.append(b_tf)
 
+        w_torch = torch.tensor(w.astype(np.float32), requires_grad=True)
+        b_torch = torch.tensor(b.astype(np.float32), requires_grad=True)
+        torch_weights_list.append(w_torch)
+        torch_biases_list.append(b_torch)
+
     x_tf = tf.constant(x.astype(np.float32))
     y_tf = tf.constant(y.astype(np.float32))
+    x_torch = torch.tensor(x.astype(np.float32))
+    y_torch = torch.tensor(y.astype(np.float32))
 
     loss = RMSELoss()
-    optimizer_tf = tf.keras.optimizers.SGD(**kwargs)
+    optimizer_tf = TF_OPTIM_MAP[optimizer_str](**kwargs)
+    new_kwargs = kwargs.copy()
+    torch_tf_map = {"learning_rate": "lr", "epsilon": "eps", "rho": "alpha"}
+    new_kwargs = {}
+    for key in kwargs:
+        try:
+            new_kwargs[torch_tf_map[key]] = kwargs[key]
+        except KeyError:
+            new_kwargs[key] = kwargs[key]
+
+    optimizer_torch = TORCH_OPTIM_MAP[optimizer_str](
+        params=[*torch_weights_list, *torch_biases_list],
+        **new_kwargs)
     for _ in range(epochs):
         # Our neural network
         feed_in = x
@@ -67,14 +103,28 @@ def test_n_hidden_layer_simple_nn(
         grads = tape.gradient(cost_tf, trainable_variables)
         optimizer_tf.apply_gradients(zip(grads, trainable_variables))
 
+        # Pytorch neural network
+        feed_in_torch = x_torch
         for idx in range(n_layers):
-            # print(dense_layers[idx]._weights, tf_weights_list[idx])
-            # for i in range(dense_layers[idx]._weights.shape[0]):
-            #     print(dense_layers[idx]._weights[i], tf_weights_list[idx][i])
-            #     print(np.allclose(
-            #         dense_layers[idx]._weights[i], tf_weights_list[idx][i]))
+            optimizer_torch.zero_grad()
+            output_torch = torch.matmul(
+                feed_in_torch,
+                torch_weights_list[idx]) + torch_biases_list[idx]
+            feed_in_torch = output_torch
+        loss_torch = torch.nn.MSELoss()
+        loss_torch_fn = torch.sqrt(loss_torch(output_torch, y_torch))
+        loss_torch_fn.backward()
+        optimizer_torch.step()
+
+        for idx in range(n_layers):
             assert np.allclose(
                 dense_layers[idx]._weights, tf_weights_list[idx])
+            assert np.allclose(
+                dense_layers[idx]._weights,
+                torch_weights_list[idx].detach().numpy())
             assert np.allclose(dense_layers[idx]._bias, tf_biases_list[idx])
-        print(cost_nn, cost_tf)
+            assert np.allclose(
+                dense_layers[idx]._bias,
+                torch_biases_list[idx].detach().numpy())
         assert np.allclose(cost_nn, cost_tf)
+        assert np.allclose(cost_nn, loss_torch_fn.item())
