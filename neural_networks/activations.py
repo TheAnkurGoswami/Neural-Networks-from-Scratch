3a609d02f78dc0a25d5bb84cdb01f8db19b48638
अंkur गोswami <kinglusifer1401@gmail.com>
2025-03-16 23:00:39 +0530

    added softmax

--- neural_networks/activations.py
+++ neural_networks/activations.py
@@ -112,6 +112,46 @@ class Tanh(Activation):
         return 1 - np.square(np.tanh(self._input))
 
 
+class Softmax(Activation):
+    def forward(self, inputs: np .ndarray):
+        """
+        inputs -> Z
+        \hat{y}_{i} = \frac{e^{z_{i}}}{\sum_{j} e^{z_{j}}}
+        output -> Y_hat
+        """
+        super().forward(inputs)
+        num = np.exp(inputs)
+        denom = np.sum(num)
+        self.activation = num / denom
+        return self.activation
+
+    def derivative(self):
+        """
+        \frac{d\hat{y}_{i}}{dz_{i}} = \hat{y}_{i} (1 - \hat{y}_{i})
+        \frac{d\hat{y}_{i}}{dz_{j}} = -\hat{y}_{i} \hat{y}_{j}
+
+        \frac{d\hat{Y}}{dZ} = J_{ij}
+        \text{J}_{ij} = \begin{cases} \hat{y}_{i} (1 - \hat{y}_{i}) & \text{if } i=j, \\ -\hat{y}_{i} \hat{y}_{j} & \text{if } i \neq j\end{cases}
+        """
+        jacobian_mat = np.zeros((self.activation.shape[1], self.activation.shape[1]))
+        for i in range(self.activation.shape[1]):
+            for j in range(i, self.activation.shape[1]):
+                if i == j:
+                    jacobian_mat[i, j] = self.activation[0][i] * (1 - self.activation[0][i])
+                else:
+                    jacobian_mat[i, j] = jacobian_mat[j, i] = -self.activation[0][i] * self.activation[0][j]
+
+        # print(jacobian_mat)
+        return jacobian_mat
+
+    def backprop(self, dA: np.ndarray):
+        """
+        dA = dL/da = dY_hat
+        """
+        jac_mat = self.derivative()
+        dZ = np.matmul(dA, jac_mat)
+        return dZ
+
 def get_activation_fn(activation: Optional[str]) -> Type[Activation]:
     """
     Get the activation function class based on the activation name.
