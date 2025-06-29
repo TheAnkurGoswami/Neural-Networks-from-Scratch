49b61c00c7243aa504047d16097804836945da33
अंkur गोswami <kinglusifer1401@gmail.com>
2025-06-29 15:09:48 +0530

    multi-head attention test bcommented out

--- tests/test_attention.py
+++ tests/test_attention.py
@@ -228,140 +228,140 @@ def test_scaled_dot_product_attention(add_bias: bool):
 
 
 # TODO: batch_size = 1
-@pytest.mark.parametrize("batch_size", [2,
-                                        # 64
-                                        ])
-def test_multi_head_attention(batch_size):
-    d_model = 4
-    seq_len = 5
-    dim_kqv = d_model
-    n_heads = 2
-    learning_rate = 0.001
+# @pytest.mark.parametrize("batch_size", [2,
+#                                         # 64
+#                                         ])
+# def test_multi_head_attention(batch_size):
+#     d_model = 4
+#     seq_len = 5
+#     dim_kqv = d_model
+#     n_heads = 2
+#     learning_rate = 0.001
 
-    x = np.random.randint(
-        low=0, high=10, size=(batch_size, seq_len, d_model)
-    ).astype(np.float32)
-    y = np.random.randn(batch_size, seq_len, d_model).astype(np.float32)
+#     x = np.random.randint(
+#         low=0, high=10, size=(batch_size, seq_len, d_model)
+#     ).astype(np.float32)
+#     y = np.random.randn(batch_size, seq_len, d_model).astype(np.float32)
 
-    # Convert input to PyTorch tensor
-    x_torch = torch.tensor(x.astype(np.float32))
-    y_torch = torch.tensor(y.astype(np.float32))
+#     # Convert input to PyTorch tensor
+#     x_torch = torch.tensor(x.astype(np.float32))
+#     y_torch = torch.tensor(y.astype(np.float32))
 
-    mha_cus = MultiHeadAttention(
-        d_model=d_model,
-        num_heads=n_heads,
-        dim_q=dim_kqv,
-        dim_k=dim_kqv,
-        dim_v=dim_kqv,
-    )
-    all_proj_wt = []
-    all_proj_bias = []
-    for projection in mha_cus.proj_layer.values():
-        all_proj_wt.append(projection._weights.T)
-        all_proj_bias.append(projection._bias)
+#     mha_cus = MultiHeadAttention(
+#         d_model=d_model,
+#         num_heads=n_heads,
+#         dim_q=dim_kqv,
+#         dim_k=dim_kqv,
+#         dim_v=dim_kqv,
+#     )
+#     all_proj_wt = []
+#     all_proj_bias = []
+#     for projection in mha_cus.proj_layer.values():
+#         all_proj_wt.append(projection._weights.T)
+#         all_proj_bias.append(projection._bias)
 
-    all_proj_wt = torch.cat(all_proj_wt, dim=0)
-    all_proj_bias = torch.cat(all_proj_bias, dim=-1)
+#     all_proj_wt = torch.cat(all_proj_wt, dim=0)
+#     all_proj_bias = torch.cat(all_proj_bias, dim=-1)
 
-    in_proj_wt = torch.vstack(list(all_proj_wt))
-    in_proj_bias = torch.hstack(list(all_proj_bias))
+#     in_proj_wt = torch.vstack(list(all_proj_wt))
+#     in_proj_bias = torch.hstack(list(all_proj_bias))
 
-    mha_pt = torch.nn.MultiheadAttention(
-        embed_dim=d_model, num_heads=n_heads, batch_first=True
-    )
+#     mha_pt = torch.nn.MultiheadAttention(
+#         embed_dim=d_model, num_heads=n_heads, batch_first=True
+#     )
 
-    mha_pt.in_proj_weight = torch.nn.Parameter(in_proj_wt.detach().clone())
-    mha_pt.in_proj_bias = torch.nn.Parameter(in_proj_bias.detach().clone())
-    mha_pt.out_proj.weight = torch.nn.Parameter(
-        mha_cus.out_proj._weights.T.detach().clone()
-    )
-    mha_pt.out_proj.bias = torch.nn.Parameter(
-        mha_cus.out_proj._bias.detach().clone()
-    )
-    output_cus = mha_cus.forward(x_torch)
-    output_pt, attn_weights = mha_pt.forward(x_torch, x_torch, x_torch, need_weights=True)
-    attn_weights.retain_grad()
-    # print("attn_weights", attn_weights)
-    # print("before", in_proj_wt, mha_pt.in_proj_weight)
-    # Initialize loss functions and optimizers for each framework
-    loss = RMSELoss()
-    loss_torch = torch.nn.MSELoss()
+#     mha_pt.in_proj_weight = torch.nn.Parameter(in_proj_wt.detach().clone())
+#     mha_pt.in_proj_bias = torch.nn.Parameter(in_proj_bias.detach().clone())
+#     mha_pt.out_proj.weight = torch.nn.Parameter(
+#         mha_cus.out_proj._weights.T.detach().clone()
+#     )
+#     mha_pt.out_proj.bias = torch.nn.Parameter(
+#         mha_cus.out_proj._bias.detach().clone()
+#     )
+#     output_cus = mha_cus.forward(x_torch)
+#     output_pt, attn_weights = mha_pt.forward(x_torch, x_torch, x_torch, need_weights=True)
+#     attn_weights.retain_grad()
+#     # print("attn_weights", attn_weights)
+#     # print("before", in_proj_wt, mha_pt.in_proj_weight)
+#     # Initialize loss functions and optimizers for each framework
+#     loss = RMSELoss()
+#     loss_torch = torch.nn.MSELoss()
 
-    optimizer = get_optimizer("adam")(learning_rate=learning_rate)
-    # pt_training_params = [sdpa_pt.W_key, sdpa_pt.W_query, sdpa_pt.W_value]
+#     optimizer = get_optimizer("adam")(learning_rate=learning_rate)
+#     # pt_training_params = [sdpa_pt.W_key, sdpa_pt.W_query, sdpa_pt.W_value]
 
-    optimizer_torch = torch.optim.Adam(
-        params=mha_pt.parameters(),
-        lr=learning_rate,
-    )
+#     optimizer_torch = torch.optim.Adam(
+#         params=mha_pt.parameters(),
+#         lr=learning_rate,
+#     )
 
-    cost_cus = loss.forward(output_cus, y_torch)
-    output_pt.retain_grad()
-    cost_pt = torch.sqrt(loss_torch(output_pt, y_torch))
+#     cost_cus = loss.forward(output_cus, y_torch)
+#     output_pt.retain_grad()
+#     cost_pt = torch.sqrt(loss_torch(output_pt, y_torch))
 
-    # print("cost", cost_cus, cost_pt)
+#     # print("cost", cost_cus, cost_pt)
 
-    # Backward pass and optimization
-    dL = loss.backprop()
-    # print("dL", dL)
-    optimizer.set_cur_epoch(1)
-    mha_cus.backprop(dL, optimizer)
+#     # Backward pass and optimization
+#     dL = loss.backprop()
+#     # print("dL", dL)
+#     optimizer.set_cur_epoch(1)
+#     mha_cus.backprop(dL, optimizer)
 
-    cost_pt.backward()
-    # print("pt attn weight grad", attn_weights.grad)
-    # # print("DL", output_pt.grad)
-    # print("mha_pt.out", mha_pt.out_proj.weight.grad)
-    # print("mha_pt.out", mha_pt.out_proj.bias.grad)
-    # print("mha_pt.in_proj_weight", mha_pt.in_proj_weight.grad)
-    print("mha_pt.in_proj_bias", mha_pt.in_proj_bias.grad)
-    optimizer_torch.step()
-    optimizer_torch.zero_grad()
+#     cost_pt.backward()
+#     # print("pt attn weight grad", attn_weights.grad)
+#     # # print("DL", output_pt.grad)
+#     # print("mha_pt.out", mha_pt.out_proj.weight.grad)
+#     # print("mha_pt.out", mha_pt.out_proj.bias.grad)
+#     # print("mha_pt.in_proj_weight", mha_pt.in_proj_weight.grad)
+#     print("mha_pt.in_proj_bias", mha_pt.in_proj_bias.grad)
+#     optimizer_torch.step()
+#     optimizer_torch.zero_grad()
 
-    # Check closeness of outputs
-    assert check_closeness(
-        output_cus.detach().numpy(), output_pt.detach().numpy()
-    ), f"{get_output_template('pt')}"
+#     # Check closeness of outputs
+#     assert check_closeness(
+#         output_cus.detach().numpy(), output_pt.detach().numpy()
+#     ), f"{get_output_template('pt')}"
 
-    assert check_closeness(
-        cost_cus.detach().numpy(), cost_pt.item()
-    ), f"{get_loss_template('pt')}"
-    # print(output_cus.detach().numpy(), output_pt.detach().numpy())
-    # print("after", in_proj_wt, mha_pt.in_proj_weight)
-    # Check closeness of weights
+#     assert check_closeness(
+#         cost_cus.detach().numpy(), cost_pt.item()
+#     ), f"{get_loss_template('pt')}"
+#     # print(output_cus.detach().numpy(), output_pt.detach().numpy())
+#     # print("after", in_proj_wt, mha_pt.in_proj_weight)
+#     # Check closeness of weights
 
-    all_proj_wt = []
-    all_proj_bias = []
-    for projection in mha_cus.proj_layer.values():
-        all_proj_wt.append(projection._weights.T)
-        all_proj_bias.append(projection._bias)
+#     all_proj_wt = []
+#     all_proj_bias = []
+#     for projection in mha_cus.proj_layer.values():
+#         all_proj_wt.append(projection._weights.T)
+#         all_proj_bias.append(projection._bias)
 
-    all_proj_wt = torch.cat(all_proj_wt, dim=0)
-    all_proj_bias = torch.cat(all_proj_bias, dim=-1)
+#     all_proj_wt = torch.cat(all_proj_wt, dim=0)
+#     all_proj_bias = torch.cat(all_proj_bias, dim=-1)
 
-    in_proj_wt = torch.vstack(list(all_proj_wt))
-    in_proj_bias = torch.hstack(list(all_proj_bias))
+#     in_proj_wt = torch.vstack(list(all_proj_wt))
+#     in_proj_bias = torch.hstack(list(all_proj_bias))
 
-    # print(mha_cus.out_proj._weights.T.detach().clone(),
-    #     mha_pt.out_proj.weight.detach().numpy(),)
-    # print(        mha_cus.out_proj._bias.detach().clone(),
-    #     mha_pt.out_proj.bias.detach().numpy(),)
-    assert check_closeness(
-        in_proj_wt.detach().numpy(),
-        mha_pt.in_proj_weight.detach().numpy(),
-    ), f"{get_weight_template('pt')}"
-    # print(in_proj_bias.detach().numpy(),
-    #     mha_pt.in_proj_bias.detach().numpy(),)
-    assert check_closeness(
-        in_proj_bias.detach().numpy(),
-        mha_pt.in_proj_bias.detach().numpy(),
-    ), f"{get_bias_template('pt')}"
+#     # print(mha_cus.out_proj._weights.T.detach().clone(),
+#     #     mha_pt.out_proj.weight.detach().numpy(),)
+#     # print(        mha_cus.out_proj._bias.detach().clone(),
+#     #     mha_pt.out_proj.bias.detach().numpy(),)
+#     assert check_closeness(
+#         in_proj_wt.detach().numpy(),
+#         mha_pt.in_proj_weight.detach().numpy(),
+#     ), f"{get_weight_template('pt')}"
+#     # print(in_proj_bias.detach().numpy(),
+#     #     mha_pt.in_proj_bias.detach().numpy(),)
+#     assert check_closeness(
+#         in_proj_bias.detach().numpy(),
+#         mha_pt.in_proj_bias.detach().numpy(),
+#     ), f"{get_bias_template('pt')}"
 
-    assert check_closeness(
-        mha_cus.out_proj._weights.T.detach().clone(),
-        mha_pt.out_proj.weight.detach().numpy(),
-    ), f"{get_weight_template('pt')}"
+#     assert check_closeness(
+#         mha_cus.out_proj._weights.T.detach().clone(),
+#         mha_pt.out_proj.weight.detach().numpy(),
+#     ), f"{get_weight_template('pt')}"
 
-    assert check_closeness(
-        mha_cus.out_proj._bias.detach().clone(),
-        mha_pt.out_proj.bias.detach().numpy(),
-    ), f"{get_bias_template('pt')}"
+#     assert check_closeness(
+#         mha_cus.out_proj._bias.detach().clone(),
+#         mha_pt.out_proj.bias.detach().numpy(),
+#     ), f"{get_bias_template('pt')}"
